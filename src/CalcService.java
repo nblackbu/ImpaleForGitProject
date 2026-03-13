b8c7aa72d53183d2c2e1b1e377d49f65c77ab06a
Elvina <sitdikovaelvinaa96@gmail.com>
2026-03-13 20:52:46 +0300

    Revert " edit Main class, delete method"
    This reverts commit 9a2c4e9e

--- src/CalcService.java
+++ src/CalcService.java
@@ -2,12 +2,4 @@ public class CalcService {
     public static void main(String[] args) {
         System.out.println("В мастере");
     }
-
-    public void methodForTask2() {
-        System.out.println("Hello from task2!");
-    }
-
-    public void methodForTask3() {
-        System.out.println("Hello from task-3!");
-    }
 }
