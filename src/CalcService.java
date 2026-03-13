0c400db34f594c36a5573fbe13d2d67b76a5a05e
Elvina <sitdikovaelvinaa96@gmail.com>
2026-03-13 20:41:23 +0300

    edit Main class, delete method

--- src/CalcService.java
+++ src/CalcService.java
@@ -2,4 +2,8 @@ public class CalcService {
     public static void main(String[] args) {
         System.out.println("В мастере");
     }
+
+    public void methodForTask3() {
+        System.out.println("Hello from task-3!");
+    }
 }
