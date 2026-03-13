2cea435898e191039be1865eec8d578a1cf0fb89
Elvina <sitdikovaelvinaa96@gmail.com>
2026-03-13 20:19:16 +0300

    edit Main class, delete method

--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,4 @@ public class Main {
     public void sum() {
         System.out.println("Возвращаю сумму");
     }
-
-    public void minus () {
-        System.out.println("Hello from task-1 branch");
-    }
 }
