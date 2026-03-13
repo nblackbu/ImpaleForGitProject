4365fb7ff1b4f807a1b14d1ecdb3b2dcb2961a9c
Elvina <sitdikovaelvinaa96@gmail.com>
2026-03-13 19:43:19 +0300

    1. add new class 2. edit Main class, add new method

--- src/Main.java
+++ src/Main.java
@@ -2,4 +2,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
+
+    public void sum() {
+        System.out.println("Возвращаю сумму");
+    }
 }
